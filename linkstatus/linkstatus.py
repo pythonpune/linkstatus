c852001ab69f07c6dd84a567352d274e7abf486f
Nikhil Dhandre <nik.digitronik@live.com>
2019-10-28 17:02:44 +0530

    add options of recurcive and timeout of requrest

--- linkstatus/linkstatus.py
+++ linkstatus/linkstatus.py
@@ -7,7 +7,7 @@ import requests
 from linkstatus.parser import parse_file
 
 
-def link_status(link):
+def link_status(link, timeout=5):
     """Check link status
 
     Args:
@@ -18,9 +18,9 @@ def link_status(link):
     """
 
     try:
-        status_code = requests.get(link, timeout=5).status_code
+        status_code = requests.get(link, timeout=timeout).status_code
     except requests.exceptions.SSLError:
-        status_code = requests.get(link, verify=False, timeout=5).status_code
+        status_code = requests.get(link, verify=False, timeout=timeout).status_code
     except Exception:  # noqa
         # TODO: include exception in logging
         status_code = None
@@ -29,11 +29,11 @@ def link_status(link):
     return status_code == 200, status_code
 
 
-def all_files(source):
+def all_files(source, recursive=False):
     files = []
     for src in source:
         if os.path.isdir(src):
-            files.extend([f for f in glob.glob(src + "**/**", recursive=True) if os.path.isfile(f)])
+            files.extend([f for f in glob.glob(src + "**/**", recursive=recursive) if os.path.isfile(f)])
         elif os.path.isfile(src):
             files.append(src)
         else:
@@ -43,9 +43,13 @@ def all_files(source):
 
 @click.command(help="Check Link Status")
 @click.argument("source", nargs=-1, type=click.Path())
-def main(source):
+@click.option(
+    "-r", "--recursive", is_flag=True, help="Include all files from directories recursively"
+)
+@click.option("-t", "--timeout", default=5, help="Request timeout (default 4 second)")
+def main(source, recursive, timeout):
     exit_code = 0
-    files = all_files(source)
+    files = all_files(source, recursive=recursive)
 
     for f in files:
         links = parse_file(f)
@@ -57,7 +61,7 @@ def main(source):
                 for url in link.urls:
                     # try two time at least
                     for _ in range(2):
-                        status, code = link_status(url)
+                        status, code = link_status(url, timeout)
                         if status:
                             break
 
--- linkstatus/parser.py
+++ linkstatus/parser.py
@@ -9,7 +9,7 @@ REGULAR_EXP = r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F]
 LINKS = namedtuple("LINKS", ["line", "urls"])
 
 
-def parse_line(string):
+def parse_line(line):
     """Parse links from line/string
 
     Args:
@@ -17,6 +17,8 @@ def parse_line(string):
     Returns:
         list of links
     """
+    string = line.strip()
+
     # if `noqa` (no quality assurance) marked in line then just skip that string
     if "noqa" not in string:
         html_format = markdown.markdown(string, output_format="html")
