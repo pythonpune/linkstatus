a5c35f74f1d25958fdebe0953f3c84ac39444f8a
Savitoj Singh <savsingh@redhat.com>
2020-03-28 16:01:42 +0530

    Added support for headers to avoid HTTP error 403
    Signed-off-by: Savitoj Singh <savsingh@redhat.com>

--- linkstatus/linkstatus.py
+++ linkstatus/linkstatus.py
@@ -20,11 +20,11 @@ def link_status(link, timeout=5):
     Returns:
         tuple of status (bool) and status code
     """
-
+    headers = {"User-Agent": "linkstatus/0.8"}
     try:
-        status_code = requests.get(link, timeout=timeout).status_code
+        status_code = requests.get(link, headers=headers, timeout=timeout).status_code
     except requests.exceptions.SSLError:
-        status_code = requests.get(link, verify=False, timeout=timeout).status_code
+        status_code = requests.get(link, verify=False, headers=headers, timeout=timeout).status_code
     except Exception:  # noqa
         # TODO: include exception in logging
         status_code = None
